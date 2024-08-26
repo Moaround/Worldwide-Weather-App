6d8cc0ada7b69b7b5de3ac30f2e2ad2ebd44bc28
Monique <mgggrol@gmail.com>
2024-08-26 12:09:11 +1000

    Added search functionality using the API

--- src/script.js
+++ src/script.js
@@ -1,9 +1,26 @@
-function handleSubmit(event) {
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "tb462323aaaf0beaa9691f3od034f454";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSubmit);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Brisbane City"); // Default setting when loading the page (delete h1 string and temperature)
